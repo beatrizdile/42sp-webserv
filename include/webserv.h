4e6f9f15be854434a76f797350db621ec867bf8a
Paulo Davi <paulo.araujo@cobli.co>
2024-07-27 20:52:18 -0300

    feat: add error handler

---
+++ include/webserv.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <iostream>
+
+#define BUF_SIZE 1024
+#define MAX_CLIENTS 100
+#define PORT 8081
--- src/webserv.cpp
+++ src/webserv.cpp
@@ -1,86 +1,96 @@
+#include "webserv.h"
 
-#include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>
-#include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <sys/epoll.h>
-#include <netdb.h>
-
-#define BUF_SIZE 500
-#define EPOLL_MAX_EVENTS 5
-
-int main()
-{
-    int socket_fd;
+int main() {
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     char *buf = (char *)malloc(BUF_SIZE);
     struct sockaddr_in server_addr;
-
-    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    struct epoll_event event;
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(8080);
+    server_addr.sin_port = htons(PORT);
 
     if (bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
         perror("bind");
         exit(1);
     };
-    if (listen(socket_fd, EPOLL_MAX_EVENTS) == -1) {
+
+    if (listen(socket_fd, MAX_CLIENTS) == -1) {
         perror("listen");
         exit(1);
     }
 
+    std::cout << "Server listening on port " << PORT << std::endl;
+
     int epoll_fd = epoll_create1(0);
-    struct epoll_event event;
+    if (epoll_fd == -1) {
+        perror("epoll_create1");
+        exit(1);
+    }
 
     event.events = EPOLLIN;
     event.data.fd = socket_fd;
-    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event);
-    
+    if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, socket_fd, &event) == -1) {
+        perror("epoll_ctl");
+        exit(1);
+    }
 
-    while (true)
-    {
-        struct epoll_event events[EPOLL_MAX_EVENTS];
-        int event_count = epoll_wait(epoll_fd, events, EPOLL_MAX_EVENTS, -1);
+    while (true) {
+        struct epoll_event events[MAX_CLIENTS];
+        int event_count = epoll_wait(epoll_fd, events, MAX_CLIENTS, -1);
+        if (event_count == -1) {
+            perror("epoll_wait");
+            exit(1);
+        }
 
         for (int i = 0; i < event_count; i++) {
             if (events[i].data.fd == socket_fd) {
                 int client_socket = accept(socket_fd, NULL, NULL);
-                std::cout << "New connection with client " << client_socket << std::endl;
+                if (client_socket == -1) {
+                    perror("accept");
+                    continue;
+                }
 
+                std::cout << "New connection with client " << client_socket << std::endl;
                 int flags = fcntl(client_socket, F_GETFL, 0);
                 fcntl(client_socket, F_SETFL, flags | O_NONBLOCK);
 
-                event.events = EPOLLIN | EPOLLET;
+                event.events = EPOLLIN | EPOLLHUP | EPOLLERR | EPOLLRDHUP;
                 event.data.fd = client_socket;
-                epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &event);
-            }
-            else if (events[i].events & EPOLLHUP || events[i].events & EPOLLERR) {
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_socket, &event) == -1) {
+                    perror("epoll_ctl");
+                }
+            } else if (events[i].events & EPOLLHUP || events[i].events & EPOLLERR || events[i].events & EPOLLRDHUP) {
                 std::cout << "Connection closed with client file descriptor " << events[i].data.fd << std::endl;
-                epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+                if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL) == -1) {
+                    perror("epoll_ctl");
+                }
                 close(events[i].data.fd);
-            }
-            else if (events[i].events & EPOLLIN) {
+            } else if (events[i].events & EPOLLIN) {
                 int bytes_read = recv(events[i].data.fd, buf, BUF_SIZE, 0);
-                if (bytes_read == 0) {
-                    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL);
+                if (bytes_read == -1) {
+                    perror("recv");
+                    continue;
+                } else if (bytes_read == 0) {
+                    std::cout << "Connection closed with client file descriptor " << events[i].data.fd << std::endl;
+                    if (epoll_ctl(epoll_fd, EPOLL_CTL_DEL, events[i].data.fd, NULL) == -1) {
+                        perror("epoll_ctl");
+                    }
                     close(events[i].data.fd);
                     continue;
                 }
-                buf[bytes_read] = '\0'; 
+                buf[bytes_read] = '\0';
                 printf("%s\n", buf);
-                send(events[i].data.fd, "HTTP/1.1 200 OK\n", 16, 0);
-                send(events[i].data.fd, "Content-length: 46\n", 19, 0);
-                send(events[i].data.fd, "Content-Type: text/html\n\n", 25, 0);
-                send(events[i].data.fd, "<html><body><H1>Hello world</H1></body></html>", 46, 0);
+                if (send(events[i].data.fd, "HTTP/1.1 200 OK\nContent-length: 46\nContent-Type: text/html\n\n<html><body><H1>Hello world</H1></body></html>", 107, 0) == -1) {
+                    perror("send");
+                }
+            } else {
+                std::cout << "Unexpected event " << events[i].events << std::endl;
             }
         }
     }
 
     free(buf);
+    close(epoll_fd);
+    close(socket_fd);
 }
